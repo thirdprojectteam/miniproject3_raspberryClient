8af0a059d5097d101b5b6ac08e4450ab5193e7fc
yang <didwnsgur999@naver.com>
2025-08-01 10:38:29 +0900

    250801 yang piezo oneshot alarm

---
+++ piezo.cpp
@@ -0,0 +1,35 @@
+#include "piezo.h"
+#define piezo_pin 17
+#include <QDebug>
+
+Piezo::Piezo(QObject *parent)
+    :QObject(parent),buzzing(false)
+{
+    m_chip = gpiod_chip_open_by_name("gpiochip0");
+    m_piezo = gpiod_chip_get_line(m_chip,piezo_pin);
+}
+
+Piezo::~Piezo(){
+    qDebug()<<"piezo deleted!";
+    gpiod_line_request_output(m_piezo,"piezo",0);
+    gpiod_line_release(m_piezo);
+    gpiod_chip_close(m_chip);
+}
+
+void Piezo::turnOnBuzzerOnce(){
+    qDebug()<<"entered"<<buzzing;
+    if(!buzzing){
+        qDebug()<<"turn on buzzer onec";
+        gpiod_line_request_output(m_piezo,"piezo",1);
+        buzzing=true;
+        QTimer::singleShot(1000,this,[this](){
+            gpiod_line_set_value(m_piezo,0);
+            gpiod_line_release(m_piezo);
+            qDebug()<<"singleshot";
+            buzzing = false;
+        });
+    }
+}
+
+
+
---
+++ piezo.h
@@ -0,0 +1,22 @@
+#ifndef PIEZO_H
+#define PIEZO_H
+
+#include <QTimer>
+#include <QObject>
+#include <gpiod.h>
+
+class Piezo: public QObject
+{
+    Q_OBJECT
+public:
+    explicit Piezo(QObject *parent = nullptr);
+    ~Piezo();
+    void turnOnBuzzerOnce();
+
+private:
+    bool buzzing;
+    gpiod_chip *m_chip;
+    gpiod_line *m_piezo;
+};
+
+#endif // PIEZO_H
