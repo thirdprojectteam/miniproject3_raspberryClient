8d5918eb144e63dd4b793bea7b116b01e22fb0fd
yang <didwnsgur999@naver.com>
2025-08-20 10:44:13 +0900

    modify ui by syl

--- mycore/main.cpp
+++ mycore/main.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    //w.show();
+    w.setWindowFlags(Qt::FramelessWindowHint);
+    w.setCursor(Qt::BlankCursor);
+    w.showFullScreen();
     return a.exec();
 }
